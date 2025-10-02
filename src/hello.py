733a04a0df095c3adad7db74bc8188ca5fbf9b65
tiohulk <tiopsn@gmail.com>
2025-10-01 21:13:11 -0300

    tdd red

---
+++ src/hello.py
@@ -0,0 +1,4 @@
+# src/hello.py
+
+def hello_world():
+    return "Hola Mundo"  # Intencionalmente incorrecto para que el test falle
---
+++ src/test/test_hello.py
@@ -0,0 +1,6 @@
+# test/test_hello.py
+
+from src.hello import hello_world
+
+def test_hello_world():
+    assert hello_world() == "Hello World"
